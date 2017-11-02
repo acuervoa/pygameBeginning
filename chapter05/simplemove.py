eaa88f2db425a9396cb24b5898accafdcbf17e17
andres.cuervo <andres.cuervo@tecnilogica.com>
2017-11-02 21:59:38 +0100

    first commit

---
+++ chapter05/simplemove.py
@@ -0,0 +1,32 @@
+background_image_filename = 'shusiplate.jpg'
+sprite_image_filename = 'fugu.png'
+
+import pygame
+from pygame.locals import *
+from sys import exit
+from random import randint
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480), 0, 32)
+
+background = pygame.image.load(background_image_filename).convert()
+sprite = pygame.image.load(sprite_image_filename)
+
+# The x coordinate of our sprote
+x = 0.
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+
+    screen.blit(background, (0, 0))
+    screen.blit(sprite, (x, 100))
+    x += 10.
+
+    # If the image goes off the end of the screen, move it back
+    if x > 640.:
+        x -= 640
+
+    pygame.display.update()
